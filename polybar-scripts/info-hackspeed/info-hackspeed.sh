ec9e0bd2f35a08baae7b2c632ce19a8dc40f0ef9
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-08-20 14:04:39 -0400

    customizing to razer keyboard needs

--- polybar-scripts/info-hackspeed/info-hackspeed.sh
+++ polybar-scripts/info-hackspeed/info-hackspeed.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # shellcheck disable=SC2016,SC2059
 
-KEYBOARD_ID="AT Translated Set 2 keyboard"
+KEYBOARD_ID=17
 
 # cpm: characters per minute
 # wpm: words per minute (1 word = 5 characters)
 METRIC=cpm
-FORMAT="# %d $METRIC"
+FORMAT="%d $METRIC"
 
 INTERVAL=20
 
@@ -14,15 +14,18 @@ INTERVAL=20
 # yourself. $3 is the key index. Use `xinput test "AT Translated Set 2 keyboard"`
 # to see key codes in real time.  Be sure to open a pull request for your
 # layout's condition!
-LAYOUT=qwerty
+LAYOUT=dvorak
 
 case "$LAYOUT" in
-	qwerty) CONDITION='($3 >= 10 && $3 <= 19) || ($3 >= 24 && $3 <= 33) || ($3 >= 37 && $3 <= 53) || ($3 >= 52 && $3 <= 58)'; ;;
-	azerty) CONDITION='($3 >= 10 && $3 <= 19) || ($3 >= 24 && $3 <= 33) || ($3 >= 37 && $3 <= 54) || ($3 >= 52 && $3 <= 57)'; ;;
-	qwertz) CONDITION='($3 >= 10 && $3 <= 20) || ($3 >= 24 && $3 <= 34) || ($3 == 36) || ($3 >= 38 && $3 <= 48) || ($3 >= 52 && $3 <= 58)'; ;;
-    dvorak) CONDITION='($3 >= 10 && $3 <= 19) || ($3 >= 27 && $3 <= 33) || ($3 >= 38 && $3 <= 47) || ($3 >= 53 && $3 <= 61)'; ;;
-    dontcare) CONDITION='1'; ;; # Just register all key presses, not only letters and numbers
-	*) echo "Unsupported layout \"$LAYOUT\""; exit 1; ;;
+qwerty) CONDITION='($3 >= 10 && $3 <= 19) || ($3 >= 24 && $3 <= 33) || ($3 >= 37 && $3 <= 53) || ($3 >= 52 && $3 <= 58)' ;;
+azerty) CONDITION='($3 >= 10 && $3 <= 19) || ($3 >= 24 && $3 <= 33) || ($3 >= 37 && $3 <= 54) || ($3 >= 52 && $3 <= 57)' ;;
+qwertz) CONDITION='($3 >= 10 && $3 <= 20) || ($3 >= 24 && $3 <= 34) || ($3 == 36) || ($3 >= 38 && $3 <= 48) || ($3 >= 52 && $3 <= 58)' ;;
+dvorak) CONDITION='($3 >= 10 && $3 <= 19) || ($3 >= 27 && $3 <= 33) || ($3 >= 38 && $3 <= 47) || ($3 >= 53 && $3 <= 61)' ;;
+dontcare) CONDITION='1' ;; # Just register all key presses, not only letters and numbers
+*)
+	echo "Unsupported layout \"$LAYOUT\""
+	exit 1
+	;;
 esac
 
 # We have to account for the fact we're not listening a whole minute
@@ -30,18 +33,21 @@ multiply_by=60
 divide_by=$INTERVAL
 
 case "$METRIC" in
-	wpm) divide_by=$((divide_by * 5)); ;;
-	cpm) ;;
-	*) echo "Unsupported metric \"$METRIC\""; exit 1; ;;
+wpm) divide_by=$((divide_by * 5)) ;;
+cpm) ;;
+*)
+	echo "Unsupported metric \"$METRIC\""
+	exit 1
+	;;
 esac
 
 hackspeed_cache="$(mktemp -p '' hackspeed_cache.XXXXX)"
 trap 'rm "$hackspeed_cache"' EXIT
 
 # Write a dot to our cache for each key press
-printf '' > "$hackspeed_cache"
-xinput test "$KEYBOARD_ID" | \
-	stdbuf -o0 awk '$1 == "key" && $2 == "press" && ('"$CONDITION"') {printf "."}' >> "$hackspeed_cache" &
+printf '' >"$hackspeed_cache"
+xinput test "$KEYBOARD_ID" |
+	stdbuf -o0 awk '$1 == "key" && $2 == "press" && ('"$CONDITION"') {printf "."}' >>"$hackspeed_cache" &
 
 while true; do
 	# Ask the kernel how big the file is with the command `stat`. The number we
@@ -52,7 +58,7 @@ while true; do
 
 	# Truncate the cache file so that in the next iteration, we count only new
 	# keypresses
-	printf '' > "$hackspeed_cache"
+	printf '' >"$hackspeed_cache"
 
 	# The shell only does integer operations, so make sure to first multiply and
 	# then divide
